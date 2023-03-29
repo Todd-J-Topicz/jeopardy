5d8dd21d6edc99af453f8ce54b656d7d644850d0
Todd-J-Topicz <tjtopicz@gmail.com>
2023-03-29 16:40:34 -0500

    Some changes....apparently

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
 import { categories } from "../testdata.jsx";
 import Gameboard from "./Gameboard.jsx";
 import Scoreboard from "./Scoreboard.jsx";
